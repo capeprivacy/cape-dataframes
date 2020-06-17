19ba8c264f147648d729da57518ce2fbd978096f
jvmancuso <jason@manc.us>
2020-06-17 11:53:06 -0400

    fix up cape_pandas types & import path

--- cape_pandas/types.py
+++ cape_pandas/types.py
@@ -1,15 +1,16 @@
 import numpy as np
+import pandas as pd
 from datetime import date
 
-String = str
-Date = date
+String = pd.api.types.pandas_dtype(str)
+Date = pd.api.types.pandas_dtype('datetime64')
 # numeric types
-Float = np.float64
-Double = np.double
-Byte = np.byte
-Short = np.short
-Integer = np.int64
-Long = np.int_
+Float = pd.api.types.pandas_dtype(np.float32)
+Double = pd.api.types.pandas_dtype(np.float64)
+Byte = pd.api.types.pandas_dtype(np.byte)
+Short = pd.api.types.pandas_dtype(np.short)
+Integer = pd.api.types.pandas_dtype(np.int32)
+Long = pd.api.types.pandas_dtype(np.int64)
 # groupings
 Floats = (Float, Double)
 Integers = (Byte, Short, Integer, Long)
