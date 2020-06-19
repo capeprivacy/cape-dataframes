91c11f6ef960379de9875acaf348a05c75ca5f00
jvmancuso <jason@manc.us>
2020-06-18 23:06:28 -0400

    add tests for rounding & numeric ptb

---
+++ cape_privacy/spark/transformations/perturbation_test.py
@@ -0,0 +1,54 @@
+import datetime
+
+import numpy as np
+import pandas as pd
+from pyspark import sql
+
+from cape_privacy.spark import dtypes
+from cape_privacy.spark import test_utils
+from cape_privacy.spark.transformations import perturbation as ptb
+
+
+def _make_and_apply_numeric_ptb(sess, df, dtype, min, max, seed=42):
+    # TODO the inferred type seems to be incorrect here
+    df = sess.createDataFrame(df, schema=['data'])
+    import pdb; pdb.set_trace()
+    perturb = ptb.NumericPerturbation(dtype, min=min, max=max, seed=seed)
+    result_df = df.select(perturb(sql.functions.col('data')))
+    return result_df.toPandas()
+
+
+def test_float_ptb_bounds():
+    sess = test_utils.make_session('test.perturbation.float.bounds')
+    data = np.arange(6, dtype=np.float32).reshape((6, 1))
+    test_df = pd.DataFrame(data, columns=['data'])
+    lower_bound = -2
+    upper_bound = 2
+    result_df = _make_and_apply_numeric_ptb(
+        sess, test_df, dtypes.Float, lower_bound, upper_bound)
+    result = result_df.values
+    assert result.dtype == data.dtype
+    noise = result - data
+    lower_check = noise >= lower_bound
+    upper_check = noise <= upper_bound
+    assert lower_check.all()
+    assert upper_check.all()
+
+
+def test_double_ptb_bounds():
+    sess = test_utils.make_session('test.perturbation.double.bounds')
+    data = np.arange(6, dtype=np.float64).reshape((6, 1))
+    test_df = pd.DataFrame(data, columns=['data'])
+    lower_bound = -2
+    upper_bound = 2
+    result_df = _make_and_apply_numeric_ptb(
+        sess, test_df, dtypes.Double, lower_bound, upper_bound)
+    result = result_df.values
+    assert result.dtype == data.dtype
+    noise = result - data
+    lower_check = noise >= lower_bound
+    upper_check = noise <= upper_bound
+    assert lower_check.all()
+    assert upper_check.all()
+
+# TODO test integer types
