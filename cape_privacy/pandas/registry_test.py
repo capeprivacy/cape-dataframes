d9f6b2c1038b129b665d7559865afeabd31a8f4a
jvmancuso <jason@manc.us>
2020-06-22 00:39:45 -0400

    standardize registries

--- cape_privacy/pandas/registry_test.py
+++ cape_privacy/pandas/registry_test.py
@@ -1,7 +1,18 @@
-from cape_privacy.pandas.transformations import get
+from cape_privacy.pandas import registry
+from cape_privacy.pandas.transformations import test_utils
 
 
 def test_get():
-    ctor = get("plusN")
+    registry.register("plusN", test_utils.PlusN)
+    ctor = registry.get("plusN")
     args = {"n": 1}
     ctor(**args)
+
+
+def test_get_missing():
+    try:
+        ctor = registry.get("plusWhat?")
+    except:
+        raise AssertionError
+    else:
+        assert ctor is None
