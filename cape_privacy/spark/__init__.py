31df755c8c865b2ca6e29c982a8bf71898790b48
Morten Dahl <mortendahlcs@gmail.com>
2020-06-22 23:04:14 +0200

    remove pyspark dependency

--- cape_privacy/spark/__init__.py
+++ cape_privacy/spark/__init__.py
@@ -1,7 +1,29 @@
-from pyspark.sql import DataFrame
+import importlib
 
-from cape_privacy.spark import dtypes
-from cape_privacy.spark import transformations
-from cape_privacy.spark.policy import apply_policies
+if importlib.util.find_spec("pyspark") is None:
 
-__all__ = ["apply_policies", "DataFrame", "dtypes", "transformations"]
+    def is_available():
+        return False
+
+    __all__ = ["is_available"]
+
+else:
+
+    from pyspark.sql import DataFrame
+
+    from cape_privacy.spark import dtypes
+    from cape_privacy.spark import transformations
+    from cape_privacy.spark.policy import apply_policies
+
+    def is_available():
+        return True
+
+    __all__ = [
+        "apply_policies",
+        "DataFrame",
+        "dtypes",
+        "is_available",
+        "transformations",
+    ]
+
+del importlib
