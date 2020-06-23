c27a33a95f0d3bfd6c4546f3b82113abd5a947a0
Morten Dahl <mortendahlcs@gmail.com>
2020-06-23 14:34:56 +0200

    tokenizer docstrings

--- cape_privacy/spark/transformations/tokenizer.py
+++ cape_privacy/spark/transformations/tokenizer.py
@@ -10,6 +10,22 @@ from cape_privacy.utils import typecheck
 
 
 class Tokenizer(base.Transformation):
+    """Tokenizer: map a string to a token to obfuscate it.
+
+    When applying the tokenizer to a Spark series of type string,
+    each value gets mapped to a token (hexadecimal string).
+    If a value is repeated several times across the series, it always
+    get mapped to the same token in order to maintain the count.
+    A value can be mapped to different tokens by setting the key to a
+    different value.
+
+    Attributes:
+        max_token_len (int or bytes): control the token length (default
+            length is 64)
+        key: expect a string or byte string. if not specified, key will
+            be set to a random byte string.
+    """
+
     identifier = "tokenizer"
     type_signature = "col->col"
 
