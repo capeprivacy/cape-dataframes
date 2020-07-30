9ff3b8f6d54ec8267b7cb0317d7a2f837e42ab94
yann dupis <yann.dupis@gmail.com>
2020-07-29 17:12:39 -0700

    import with sess.read.load

--- examples/tutorials/credit/apply_policy_spark.py
+++ examples/tutorials/credit/apply_policy_spark.py
@@ -1,18 +1,23 @@
-import cape_privacy as cape
-import pandas as pd
 from pyspark import sql
+from pyspark.sql import functions
+
+import cape_privacy as cape
 
 # Set up your SparkSession as usual, but configure it for use with Cape.
 # We do this because some transformations expect Arrow to be enabled.
-sess = sql.SparkSession.builder \
-    .appName("cape.tutorial.maskPersonalInformation") \
-    .getOrCreate()
+sess = sql.SparkSession.builder.appName(
+    "cape.tutorial.maskPersonalInformation"
+).getOrCreate()
 sess = cape.spark.configure_session(sess)
 
 # Load a Spark DataFrame
-pdf = pd.read_csv('data/credit_with_pii.csv')
-pdf.Application_date = pd.to_datetime(pdf.Application_date)
-df = sess.createDataFrame(pdf)
+df = sess.read.load(
+    "data/credit_with_pii.csv", format="csv", sep=",", inferSchema="true", header="true"
+)
+df = df.withColumn(
+    "Application_date",
+    functions.to_date(functions.col("Application_date"), "yyyy-MM-dd"),
+)
 print("Original Dataset:")
 print(df.show())
 # Load the privacy policy and apply it to the DataFrame
