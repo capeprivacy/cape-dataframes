9f1b85e7822111e45f598448616e2bc3811f695a
Justin Patriquin <justinpatriquin@gmail.com>
2020-08-19 11:58:17 -0300

    add cryptodome to setup.py

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setuptools.setup(
         "numpy==1.18.1",
         "pyyaml==5.3.1",
         "validators==0.15.0",
+        "pycryptodome==3.9.8",
     ],
     extras_require={
         "spark": ["pyspark >=2.4", "pyarrow >=0.15.1"],
