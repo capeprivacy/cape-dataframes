de9adcc263f5c34433485489bb1188eecf3b48f2
jvmncs <jvmncs@gmail.com>
2023-02-16 13:49:08 -0500

    bump version from 0.3.0 to 0.3.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setuptools.setup(
     name="cape-dataframes",
-    version="0.3.0",
+    version="0.3.1",
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
     license="Apache License 2.0",
